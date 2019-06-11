acbb0bbef908fd9f14603d245241cc021a31a4da
Samuel Ueberle <s.ueberle@fu-berlin.de>
2019-06-12 00:21:59 +0200

    spa: updates components vuetify based

---
+++ components/Action/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-flex>
+        <v-card>
+            <v-card-title primary-title>
+                {{action.name}}
+            </v-card-title>
+            <v-card-text>
+                <v-layout>
+                    <v-flex>
+                        {{action.device}}
+                    </v-flex>
+                    <v-flex>
+                        {{daysOfWeek}}
+                    </v-flex>
+                    <v-flex>
+                        {{hours}}:{{minutes}}
+                    </v-flex>
+                    <v-flex>
+                        {{action.method}}
+                    </v-flex>
+                </v-layout>
+            </v-card-text>
+        </v-card>
+    </v-flex>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { IAction } from '../../interfaces';
+export default Vue.extend({
+    props: {
+        action: {
+            required: true,
+            type: Object as () => IAction
+        }
+    },
+    computed: {
+
+        daysOfWeek: function(){
+
+            let a: IAction = this.action
+            return (a.interval && a.interval.daysOfWeek.join(' | '))
+            || ""
+        },
+        hours: function(){
+
+            let a: IAction = this.action
+            return (a.interval && a.interval.hour)
+            || ""
+        },
+        minutes: function(){
+
+            let a: IAction = this.action
+            return (a.interval && a.interval.minute)
+            || ""
+        },
+        
+    }
+})
+</script>
---
+++ components/Device/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-flex xs4 class="ma-3">
+        <v-card>
+            <v-card-text>
+                <v-form>
+                    <v-text-field
+                        label= "Name"
+                        v-model.lazy="device.name"
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        label= "on"
+                        v-model.lazy="device.on"
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        label= "off"
+                        v-model.lazy="device.off"
+                    >
+                    </v-text-field>
+                </v-form>
+            </v-card-text>
+            <v-card-actions
+            >
+                <v-btn @click="del(device.id)">
+                    Delete
+                </v-btn>
+                <v-btn @click="update(device)">
+                    Update
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-flex>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { IDevice } from '../../interfaces';
+export default Vue.extend({
+    props: {
+        device: {
+            required: true,
+            type: Object as () => IDevice
+        }
+    },
+    methods: {
+        del: function(id: number){
+
+            this.$emit('del', id)
+        },
+        update: function(device: IDevice){
+
+            this.$emit('update', device)
+        }
+    }
+})
+</script>
