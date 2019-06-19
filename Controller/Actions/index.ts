b76cf48b5c51563c0d32b73af6a3170b4b6b4b36
Samuel Ueberle <s.ueberle@fu-berlin.de>
2019-06-19 23:40:01 +0200

    controller: considers action device as multiple

--- Controller/Actions/index.ts
+++ Controller/Actions/index.ts
@@ -1,14 +1,15 @@
-import { IAction } from "~/interfaces";
+import { IAction, IDevice } from "~/interfaces";
 import Axios from "axios";
 
 let calledMinute = undefined as undefined | number
 export function execAction(action: IAction){
-
-    if(action.method === 'on'){
-        
-        return Axios.get(action.device.on)
-    } else if(action.method === 'off') {
-        
-        return Axios.get(action.device.off)
+    if(action.method === 'off' || action.method === 'on'){
+        let waitables: Promise<any>[] = []
+        if(Array.isArray(action.device)){
+            waitables = action.device.map(d => Axios.get(d[action.method]))
+        } else {
+            waitables = [Axios.get(action.device[action.method])]
+        }
+        return Promise.all(waitables);
     }
 }
